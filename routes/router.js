ac8076b38cfa2d2c2e1ecf9b0eeb385c95a33d18
Hyuna <hannah3767@gmail.com>
2019-06-08 20:16:23 +0900

    Updated group_reply & group_post and Created create-single-post.ejs

--- routes/router.js
+++ routes/router.js
@@ -1,144 +1,246 @@
-module.exports = function(app){
+module.exports = function (app) {
 
-	const http = require('http');
-	const express = require('express');
-	const router = express.Router();
-	const url = require('url');
-
-
-
-	/* GET home page. */
-	app.get('/', function(req, res, next) {
-       
-        connection.query('SELECT * FROM menu_type',  (err, results) => {
-        	if (err){
-          		console.log(err);
-          		res.render('error');
-     	 	}
-     		types = results;
-     		console.log(types);
-      		connection.query('SELECT * FROM location',  (err, results) => {
-        		if (err){
-          			console.log(err);
-          			res.render('error');
-      			}
-      			locations = results;
-      			connection.query('SELECT * FROM restaurant r, location l WHERE r.idlocation=l.idlocation',  (err, results) => {
+    const http = require('http');
+    const express = require('express');
+    const router = express.Router();
+    const url = require('url');
 
 
-      				console.log(results);
-      				console.log(locations);
-      				res.render('index', {
-            		'types' : types,
-            		'locations': locations,
-            		'restaurants': results
+    /* GET home page. */
+    app.get('/', function (req, res, next) {
 
-            		});
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            console.log(types);
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
+                connection.query('SELECT * FROM restaurant r, location l WHERE r.idlocation=l.idlocation', (err, results) => {
 
 
-      			});
+                    console.log(results);
+                    console.log(locations);
+                    res.render('index', {
+                        'types': types,
+                        'locations': locations,
+                        'restaurants': results
 
-    		});
-  		});
+                    });
+
+
+                });
+
+            });
+        });
     });
 
-	app.get('/dashboard', function(req, res, next){
-		res.render('dashboard');
-	});
+    app.get('/dashboard', function (req, res, next) {
+        var locatons;
+        var types;
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
 
+                connection.query('SELECT * FROM group_post', (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        res.render('error');
+                    }
+                    posts = results;
+                    console.log(results);
+                    connection.query('SELECT * FROM group_reply', (err, results) => {
+                        if (err) {
+                            console.log(err);
+                            res.render('error');
+                        }
+                        connection.query('SELECT * FROM group_post gp, group_reply gr WHERE gp.idgroup_post=gr.idgroup_post', (err, results) => {
+                            console.log(results);
+                            res.render('dashboard', {
+                                'types': types,
+                                'posts': results,
+                                'restaurants': results
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 
-	app.get('/error', function(req, res, next){
-		res.render('error');
-	});
+    app.get('/single-board', function (req, res, next) {
+        var locatons;
+        var types;
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
 
+                connection.query('SELECT * FROM group_post', (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        res.render('error');
+                    }
+                    posts = results;
+                    console.log(results);
+                    connection.query('SELECT * FROM group_reply', (err, results) => {
+                        if (err) {
+                            console.log(err);
+                            res.render('error');
+                        }
+                        connection.query('SELECT * FROM group_post gp, group_reply gr WHERE gp.idgroup_post=gr.idgroup_post', (err, results) => {
+                            console.log(results);
+                            res.render('single-board', {
+                                'types': types,
+                                'posts': results,
+                                'restaurants': results
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 
-	app.get('/category/:categoryid', function(req, res, next) {
+    app.get('/create-single-dashboard', function (req, res, next) {
+        var locatons;
+        var types;
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
 
-  		var body = req.body;
-   		var searchword = body.searchword;
-   		console.log(searchword);
-   		var locatons;
-       	var types;
-        connection.query('SELECT * FROM menu_type',  (err, results) => {
-        	if (err){
-          		console.log(err);
-          		res.render('error');
-     	 	}
-     		types = results;
-      		connection.query('SELECT * FROM location',  (err, results) => {
-        		if (err){
-          			console.log(err);
-          			res.render('error');
-      			}
-      			locations = results;
-      			connection.query('SELECT * FROM restaurant WHERE idmenu_type LIKE ?', [req.params.categoryid],  (err, results) => {
-        		if (err){
-          			console.log(err);
-          			res.render('error');
-      			}
+                console.log(results);
 
-      			console.log(results);
-      			
-      			res.render('category', {
-            		'types' : types,
-            		'locations': locations,
-            		'restaurants': results,
-      			});
+                res.render('create-single-dashboard', {
+                    'types': types,
+                    'locations': locations
+                });
 
-    			});
-  			});
-    	});
+            });
+        });
+    });
+
+    app.get('/error', function (req, res, next) {
+        res.render('error');
     });
 
 
-  	app.get('/user', function(req, res, next) {
-		res.render('user-profile');
-  	});
+    app.get('/category/:categoryid', function (req, res, next) {
 
+        var body = req.body;
+        var searchword = body.searchword;
+        console.log(searchword);
+        var locatons;
+        var types;
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
+                connection.query('SELECT * FROM restaurant WHERE idmenu_type LIKE ?', [req.params.categoryid], (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        res.render('error');
+                    }
 
-  	app.post('/do_search', function(req, res, next) {
+                    console.log(results);
 
-  		var body = req.body;
-   		var searchword = body.searchword;
-   		console.log(searchword);
-   		var locatons;
-       	var types;
-        connection.query('SELECT * FROM menu_type',  (err, results) => {
-        	if (err){
-          		console.log(err);
-          		res.render('error');
-     	 	}
-     		types = results;
-      		connection.query('SELECT * FROM location',  (err, results) => {
-        		if (err){
-          			console.log(err);
-          			res.render('error');
-      			}
-      			locations = results;
-      			connection.query('SELECT * FROM restaurant WHERE r_name LIKE ?', ['%' + searchword +'%'],  (err, results) => {
-        		if (err){
-          			console.log(err);
-          			res.render('error');
-      			}
+                    res.render('category', {
+                        'types': types,
+                        'locations': locations,
+                        'restaurants': results,
+                    });
 
-      			console.log(types);
-      			console.log(locations);
-      			console.log(results);
-      			
-      			res.render('search', {
-            		'types' : types,
-            		'locations': locations,
-            		'search_results': results,
-            		'searchword': searchword
-      			});
-
-    			});
-  			});
-    	});
+                });
+            });
+        });
     });
 
 
-  	
+    app.get('/user', function (req, res, next) {
+        res.render('user-profile');
+    });
+
+
+    app.post('/do_search', function (req, res, next) {
+
+        var body = req.body;
+        var searchword = body.searchword;
+        console.log(searchword);
+        var locatons;
+        var types;
+        connection.query('SELECT * FROM menu_type', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location', (err, results) => {
+                if (err) {
+                    console.log(err);
+                    res.render('error');
+                }
+                locations = results;
+                connection.query('SELECT * FROM restaurant WHERE r_name LIKE ?', ['%' + searchword + '%'], (err, results) => {
+                    if (err) {
+                        console.log(err);
+                        res.render('error');
+                    }
+
+                    console.log(types);
+                    console.log(locations);
+                    console.log(results);
+
+                    res.render('search', {
+                        'types': types,
+                        'locations': locations,
+                        'search_results': results,
+                        'searchword': searchword
+                    });
+
+                });
+            });
+        });
+    });
 
 
 }
