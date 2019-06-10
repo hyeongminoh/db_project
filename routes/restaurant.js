5a5e95d054cdbaa2e50f7156b632123d1e7db735
kathryn-choi <mody135@gmail.com>
2019-06-11 02:20:27 +0900

    Review insert fixed & view page edited

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,37 +1,34 @@
-module.exports = function(app) {
+module.exports = function(app){
 
     const http = require('http');
     const express = require('express');
     const router = express.Router();
-    const url = require('url');
+	const url = require('url');
+	const util = require('util');
     var bodyParser = require('body-parser');
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.urlencoded({extended :false}));
 
-    app.get('/restaurant/:restaurantid', function (req, res, next) {
-
-        const http = require('http');
-        const express = require('express');
-        const router = express.Router();
-        const url = require('url');
-        var bodyParser = require('body-parser');
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: false}));
-
-        app.get('/restaurant/:restaurantid', function (req, res, next) {
-            var sqlquery = "SELECT  * FROM restaurant WHERE idrestaurant=?";
-            const sess = req.session;
-            console.log("restaurantid:", req.params.restaurantid)
-            var name, location, time, menu_type, reservation, phone, id_menu_type, idlocation, walk_time, image
-            var idrestaurant
-            var menu = []
-            var types;
-            var locatons;
-            var content, review, star, like
-            var review = []
-            var idrestaurant
-            connection.query('SELECT * FROM menu_type', (err, results) => {
-                if (err) {
+	app.get('/restaurant/:restaurantid', function(req, res, next) {
+        var sqlquery =  "SELECT  * FROM restaurant WHERE idrestaurant=?";
+        const sess = req.session;
+        console.log("restaurantid:",req.params.restaurantid)
+        var name,location,time,menu_type,reservation,phone,id_menu_type, idlocation,walk_time,image
+                var idrestaurant
+        var menu = []
+        var types;
+        var locatons;
+                var content,review,star,like
+                var review = []
+                var idrestaurant
+        connection.query('SELECT * FROM menu_type',  (err, results) => {
+            if (err){
+                console.log(err);
+                res.render('error');
+            }
+            types = results;
+            connection.query('SELECT * FROM location',  (err, results) => {
+                if (err){
                     console.log(err);
                     res.render('error');
                 }
@@ -68,117 +65,63 @@ module.exports = function(app) {
                                             }
                                             menu.push(temp)
                                         }
-                                        var sqlquery3 = "SELECT  * FROM location WHERE idlocation=?";
-                                        connection.query(sqlquery3, idlocation, function (err, row) {
-                                            walk_time = row[0].time
-                                            var info = {
-                                                idrestaurant: idrestaurant,
-                                                name: name,
-                                                location: location,
-                                                time: time,
-                                                menu_type: menu_type,
-                                                reservation: reservation,
-                                                phone: phone,
-                                                walk_time: walk_time,
-                                                image: image
-                                            }
-                                            console.log(info)
-                                            console.log(menu)
+                                    var sqlquery3 =  "SELECT  * FROM location WHERE idlocation=?";
+                                    connection.query(sqlquery3, idlocation, function (err, row) {
+                                        walk_time = row[0].time
+                                        var info = {
+                                            idrestaurant : idrestaurant ,
+                                            name : name,
+                                            location : location,
+                                            time : time,
+                                            menu_type: menu_type,
+                                            reservation : reservation,
+                                            phone : phone,
+                                            walk_time : walk_time,
+                                            image : image
+                                        }
+                                        console.log(info)
+                                        console.log(menu)
+										var sqlquery4 ="SELECT * FROM review WHERE idrestaurant=?";
+										var review = []
+										var count = 0
+										connection.query(sqlquery4, req.params.restaurantid ,function(err, row)  {
+										if (err){
+												console.log(err)
+												console.log('리뷰오류');
+										}
+										if(row.length == 0){
+											review = []
+											res.render('restaurant',{types : types,info : info, menu : menu, review: review,session : sess});
+										}
+										for(var i =0; i<row.length; i++){
+											var temp = {
+												review: '',
+												star: '',
+												idrestaurant:'',
+												idreview : ''
+												// hashtag: []
+											}
+											temp.idrestaurant = row[i].idrestaurant
+											temp.review = row[i].review
+											temp.idreview = row[i].idreview
+											temp.star = row[i].star
+											review.push(temp)
+											var sqlquery5 ="SELECT h.content FROM hashtag h, hashtag_connection c WHERE h.idhashtag = c.idhashtag and c.idreview = ? and c.idrestaurant=?";
+											console.log("reviewid", row[i].idreview)
+											var reviewid = row[i].idreview
+											connection.query(sqlquery5, [row[i].idreview,req.params.restaurantid] ,function(err, row1)  {
+												console.log("hashtag:",row1)
+												review[count].hashtag = row1
+												count++
+												if(count == row.length){
+													console.log(review)
+													res.render('restaurant',{types : types,info : info, menu : menu, review: review,session : sess});
+												}
+											});
+										}
+									});
 
-                                            var sqlquery4 = "SELECT * FROM review WHERE idrestaurant=?";
-                                            var review = []
-                                            var count = 0
-                                            connection.query(sqlquery4, req.params.restaurantid, function (err, row) {
-                                                if (err) {
-                                                    console.log('리뷰오류');
-                                                }
-                                                for (var i = 0; i < row.length; i++) {
-                                                    var temp = {
-                                                        review: '',
-                                                        star: '',
-                                                        idrestaurant: '',
-                                                        idreview: ''
-                                                        // hashtag: []
-                                                    }
-                                                    temp.idrestaurant = row[i].idrestaurant
-                                                    temp.review = row[i].review
-                                                    temp.idreview = row[i].idreview
-                                                    temp.star = row[i].star
-                                                    review.push(temp)
-                                                    var sqlquery5 = "SELECT h.content FROM hashtag h, hashtag_connection c WHERE h.idhashtag = c.idhashtag and c.idreview = ? and c.idrestaurant=?";
-                                                    console.log("reviewid", row[i].idreview)
-                                                    var reviewid = row[i].idreview
-                                                    connection.query(sqlquery5, [row[i].idreview, req.params.restaurantid], function (err, row1) {
-                                                        console.log("hashtag:", row1)
-                                                        review[count].hashtag = row1
-                                                        // for(var k =0; k<review.length; k++){
-                                                        //  if(review[k].idreview == reviewid){
-                                                        //      for(var j =0; j<row1.length; j++){
-                                                        //          review[k].hashtag.push(row1[j].content)
-                                                        //      }
-                                                        //  }
-                                                        // }
-                                                        count++
-                                                        if (count == row.length) {
-                                                            console.log(review)
-                                                            res.render('restaurant', {
-                                                                types: types,
-                                                                info: info,
-                                                                menu: menu,
-                                                                review: review,
-                                                                session: sess
-                                                            });
-                                                        }
-                                                    });
-                                                }
-                                            });
-                                            var sqlquery4 = "SELECT * FROM review WHERE idrestaurant=?";
-                                            var review = []
-                                            var count = 0
-                                            connection.query(sqlquery4, req.params.restaurantid, function (err, row) {
-                                                if (err) {
-                                                    console.log('리뷰오류');
-                                                }
-                                                for (var i = 0; i < row.length; i++) {
-                                                    var temp = {
-                                                        review: '',
-                                                        star: '',
-                                                        idrestaurant: '',
-                                                        idreview: ''
-                                                        // hashtag: []
-                                                    }
-                                                    temp.idrestaurant = row[i].idrestaurant
-                                                    temp.review = row[i].review
-                                                    temp.idreview = row[i].idreview
-                                                    temp.star = row[i].star
-                                                    review.push(temp)
-                                                    var sqlquery5 = "SELECT h.content FROM hashtag h, hashtag_connection c WHERE h.idhashtag = c.idhashtag and c.idreview = ? and c.idrestaurant=?";
-                                                    console.log("reviewid", row[i].idreview)
-                                                    var reviewid = row[i].idreview
-                                                    connection.query(sqlquery5, [row[i].idreview, req.params.restaurantid], function (err, row1) {
-                                                        console.log("hashtag:", row1)
-                                                        review[count].hashtag = row1
-                                                        count++
-                                                        if (count == row.length) {
-                                                            console.log(review)
-                                                            res.render('restaurant', {
-                                                                types: types,
-                                                                info: info,
-                                                                menu: menu,
-                                                                review: review,
-                                                                session: sess
-                                                            });
-                                                        }
-                                                    });
-                                                }
-                                            });
-
-
-                                        })
-                                    })
-                                } else {
-                                    console.log("음식 선택 오류!")
-                                }
+                                })
                             })
 
 
@@ -186,56 +129,67 @@ module.exports = function(app) {
                             console.log("식당 선택 오류!")
                         }
 
-                    });
-                });
+					});
+				}
             });
         });
+	});
+    
+    app.post('/restaurant/:restaurantid', async(req, res,next)  => {
+	if(req.session.user_info == undefined){
+		console.log("로그인 필요!");
+		res.redirect('/restaurant/'+ req.params.restaurantid);
+	}
+	console.log(req.session.user_info.user_num)
+	var user_num = req.session.user_info.user_num
+    var body = req.body;
+	var _hashtag = body.hashtag;
+    var review = body.review;
+	var star = body.star;
+	var hashtag = []
+	_hashtag = _hashtag.toString()
+	_hashtag = _hashtag.split('#')
+	for(var i = 1; i< _hashtag.length; i++){
+		hashtag.push('#' + _hashtag[i])
+	}
+	console.log(hashtag)
+	var sqlquery2 = "insert into review(user_num,idrestaurant,review,star) values (?,?,?,?)"
+	const query = util.promisify(connection.query).bind(connection);
+	var rows = await query(sqlquery2, [user_num,req.params.restaurantid,review,star]) 
+		if (!rows.insertId) {
+			await console.log(err);
+			res.render('error');
+		}
+		else{
+			var reviewid = rows.insertId
+			await console.log("Review 테이블 insert 완료")
+				var count = 0
+				for(var j =0; j<hashtag.length; j++){
+					var selectedhashtag = hashtag[j]
+					var sqlquery4 = "insert into hashtag(content) values (?)"
+					var row1 = query(sqlquery4, selectedhashtag)
+						
+					await console.log("hashtag 테이블 insert 완료")
+					var sqlquery5 = "select * from hashtag where content = ?"
+					var row2 = await query(sqlquery5, selectedhashtag)
+					var idhashtag = row2[0].idhashtag
+					var sqlquery6 ="insert into hashtag_connection(idreview,idhashtag,idrestaurant) values (?,?,?)"
+					var row3 = query(sqlquery6, [reviewid,idhashtag,req.params.restaurantid])
+					if (!row3) {
+						console.log(err);
+						res.render('error');
+					}
+					else{
+						console.log("connection 테이블 insert 완료")
+						count++
+						if(count == hashtag.length){
+							res.redirect('/restaurant/'+ req.params.restaurantid);
+						}
+					}
+				}	
+			}
+		})
 
-        app.post('/restaurant/:restaurantid', function (req, res, next) {
-
-            var body = req.body;
-            var user_num = body.user_num;
-            var content = body.content;
-            var review = body.review;
-            var star = body.star;
-            console.log(user_num);
-            console.log(content);
-            console.log(review);
-            console.log(별);
-            console.log("restaurantid:", req.params.restaurantid);
-            var insertQuery = 'INSERT INTO hashtag (content) VALUES (?)';
-            connection.query(insertQuery, [content], function (err, results) {
-                if (err) {
-                    console.log(err);
-                    res.render('error');
-                }
-                console.log("Data inserted!1");
-                console.log(results);
-
-
-                var insertQuery2 = 'INSERT INTO review (user_num,idrestaurant,review ,star)  VALUES (?,?,?,?)';
-                connection.query(insertQuery2, [user_num, req.params.restaurantid, review, star], function (err, results) {
-                    if (err) {
-                        console.log(err);
-                        res.render('error');
-                    }
-                    console.log("Data inserted!2");
-                    console.log(results);
-
-
-                    var insertQuery3 = 'INSERT hashtag_connection (idreview,idhashtag,idrestaurant)SELECT t.idreview,h.idhashtag ,t.idrestaurant FROM review t, hashtag h WHERE t.review =? AND h.content= ?';
-                    connection.query(insertQuery3, [review, content], function (err, results) {
-                        if (err) {
-                            console.log(err);
-                            res.render('error');
-                        }
-                        console.log("Data inserted!3");
-                        console.log(results);
-                        res.redirect('/restaurant/' + req.params.restaurantid);
-
-                    });
-                });
-            });
-        });
-    });
+	})
+})
 }
